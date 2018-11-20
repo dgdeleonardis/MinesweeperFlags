8233a7b0b4b135b07899d39fd5776a602abb69d2
dgdeleonardis <diegowazzaap@hotmail.it>
2018-11-20 21:31:45 +0100

    adding some TODO-comments for the next programming session

--- GUI.java
+++ GUI.java
@@ -3,15 +3,17 @@ import javax.swing.*;
 
 public class GUI extends JFrame {
 
-	private Container container;
-	private Cella[][] prato;
+	private Container container;					//container posto come attributo per semplicità
+	private Cella[][] prato;						//matrice di celle
 
-	
+	//costruttore
 	public GUI(int righe, int colonne) {
 		super("Prato Fiorito");
-		this.prato = new Cella[righe][colonne];
-		this.container = this.getContentPane();
+
+		this.container = this.getContentPane();							//strutturazione dell'interfaccia
 		this.container.setLayout(new GridLayout(righe, colonne));
+		// TODO: inserire due pannelli, uno con il contatore e l'altro per il prato!
+		this.prato = new Cella[righe][colonne];
 		for(int i = 0; i < righe; i++) {
 			for(int j = 0; j < colonne; j++) {
 				prato[i][j] = new Cella();
@@ -21,4 +23,13 @@ public class GUI extends JFrame {
 		setVisible(true);
 		}
 	}
+
+	private void inserimentoBombe() {
+		//TODO: data la matrice prato e il container, vengono create le celle e inserite le bombe!
+	}
+	private void popolamentoPrato() {
+		//TODO: data la matrice prato con le bombe inserite, vengono popolate le celle non aventi le bombe!
+	}
+
+
 }
